fe0d53eb4ddbeaf622f7f3402c3599faf1c49155
Jeyasurya14 <jeyas865@gmail.com>
2025-07-22 03:47:19 +0530

    Day 1: Variables, Input, and Output

---
+++ 00_basics/day1_variables/day1_variables/day1_variables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your Channel name : ");
+        string name = Console.ReadLine();
+        Console.Write("Enter your Subscriber Count : ");
+        int count = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("<----- Channel Details ----->");
+        Console.WriteLine("Name : "+ name);
+        Console.WriteLine("Subscriber Count : "+ count);
+        
+    }
+}
